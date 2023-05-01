0671c18e8b406ee12f48cd8b63ed2e2d514551f5
Le Khoa Vu <vulk29@gmail.com>
2023-04-30 21:26:48 -0400

    Query 3 changed
    Signed-off-by: Le Khoa Vu <vulk29@gmail.com>

--- src/queries/use_case_Vu.sql
+++ src/queries/use_case_Vu.sql
@@ -9,15 +9,13 @@ WHERE t.FirstName = 'Long' AND t.LastName = 'Nguyen')
 -- Delete a tenant id 1007 as they stop renting an apartment
 DELETE FROM Tenant WHERE id = 1007
 
---Query all amount of money that Tenant Hieu Dang owes Owner John Doe
+--Query all amount of money that Tenant Hieu Dang owes
 SELECT Payment.tenant_id, concat(Tenant.FirstName, ' ', Tenant.LastName) as TenantName, concat(Owner.FirstName, ' ', Owner.LastName) as OwnerName, Payment.amount, Payment.due_date
 FROM Payment
 JOIN Tenant ON Payment.tenant_id = Tenant.id
 JOIN Owner ON Payment.owner_id = Owner.id
 WHERE Tenant.FirstName = 'Hieu'
 AND Tenant.LastName = 'Dang'
-AND Owner.FirstName = 'John'
-AND Owner.LastName = 'Doe'
 
 -- Query all amount of money that is overdue
 SELECT Payment.tenant_id, concat(Tenant.FirstName, ' ', Tenant.LastName) as TenantName, concat(Owner.FirstName, ' ', Owner.LastName) as OwnerName, Payment.amount, Payment.due_date,  Payment.status
