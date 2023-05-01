cea050f1abfc2dc0649823213434e6eac1cf7f27
Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>
2023-04-30 21:26:20 -0400

    change terminal design

---
+++ src/util/UseCaseImpl.java
@@ -0,0 +1,20 @@
+package util;
+
+public class UseCaseImpl {
+    public static void addRequest() {
+        // status is automatically set
+        // prompt take input: apt_id and category
+
+
+    }
+
+    public static void changeRequestStatus() {
+
+    }
+
+    public static void showUnfinishedRequests() {
+
+    }
+
+
+}
